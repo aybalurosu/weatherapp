465810d3e6a9cadf1dcae47b8f8c6fa4cdfd6dd8
aybaluro <aybalurosu@gmail.com>
2025-07-07 19:38:15 +0200

    styling popover

--- src/app/components/Popover/PopoverSettings.js
+++ src/app/components/Popover/PopoverSettings.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
-import { bindPopover } from 'material-ui-popup-state';
+import Button from '@mui/material/Button';
+
 
 export default function PopoverSettings({ anchorEl, onClose }) {
     const open = Boolean(anchorEl);
 
     return (
-        <Popover
-            open={open}
-            anchorEl={anchorEl}
-            onClose={onClose}
-            anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-            }}
-            transformOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
-            }}
-        >
-
+        <Popover open={open} anchorEl={anchorEl} onClose={onClose} anchorOrigin={{vertical: 'top',
+            horizontal: 'left',}} transformOrigin={{vertical: 'bottom', horizontal: 'left',}}
+            slotProps={{
+                paper: {
+                sx: {
+                    bgcolor: '#6b6b6ba8',
+                    p: 2,
+                    borderRadius: 2,
+                    boxShadow: 10,
+                },
+                },
+            }}>
+            <div className='flex flex-col'>
+                <Button>Setting 1</Button>
+                <Button>Setting 2</Button>
+                <Button>Setting 3</Button>
+                <Button>Setting 4</Button>
+                <Button>Setting 5</Button>
+                <Button>Setting 6</Button>
+            </div>
         </Popover>
     );
 }
